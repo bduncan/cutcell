975ee83e084a0dfe45b941bbde5d0caa0c566c8a
Bruce Duncan <Bruce.Duncan@ed.ac.uk>
2010-04-29 16:46:19 +0100

    Add make_cube_3 function from examples.

--- cutcell.cpp
+++ cutcell.cpp
@@ -7,14 +7,37 @@ typedef Kernel::Point_3 Point_3;
 typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
 typedef Polyhedron::Vertex_iterator Vertex_iterator;
 
+template <class Poly>
+typename Poly::Halfedge_handle make_cube_3( Poly& P) {
+    // appends a cube of size [0,1]^3 to the polyhedron P.
+    CGAL_precondition( P.is_valid());
+    typedef typename Poly::Point_3         Point;
+    typedef typename Poly::Halfedge_handle Halfedge_handle;
+    Halfedge_handle h = P.make_tetrahedron( Point( 1, 0, 0),
+                                            Point( 0, 0, 1),
+                                            Point( 0, 0, 0),
+                                            Point( 0, 1, 0));
+    Halfedge_handle g = h->next()->opposite()->next();             // Fig. (a)
+    P.split_edge( h->next());
+    P.split_edge( g->next());
+    P.split_edge( g);                                              // Fig. (b)
+    h->next()->vertex()->point()     = Point( 1, 0, 1);
+    g->next()->vertex()->point()     = Point( 0, 1, 1);
+    g->opposite()->vertex()->point() = Point( 1, 1, 0);            // Fig. (c)
+    Halfedge_handle f = P.split_facet( g->next(),
+                                       g->next()->next()->next()); // Fig. (d)
+    Halfedge_handle e = P.split_edge( f);
+    e->vertex()->point() = Point( 1, 1, 1);                        // Fig. (e)
+    P.split_facet( e, f->next()->next());                          // Fig. (f)
+    CGAL_postcondition( P.is_valid());
+    return h;
+}
+
+
 int main() {
-    Point_3 p( 1.0, 0.0, 0.0);
-    Point_3 q( 0.0, 1.0, 0.0);
-    Point_3 r( 0.0, 0.0, 1.0);
-    Point_3 s( 0.0, 0.0, 0.0);
     Polyhedron P;
 
-    P.make_tetrahedron( p, q, r, s);
+    make_cube_3(P);
     CGAL::set_ascii_mode( std::cout);
     for (Vertex_iterator v = P.vertices_begin(); v != P.vertices_end(); ++v)
         std::cout << v->point() << std::endl;
