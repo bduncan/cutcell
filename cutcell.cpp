fb547a1e46de154d7dbd6f3db1a4baf25c40b9e3
Bruce Duncan <Bruce.Duncan@ed.ac.uk>
2010-06-11 17:12:02 +0100

    Fix up UnitCube test case.
    This is an offset cube with corners at -0.75 and 2.25.
    Removed the make_cube_3 function, switch back to
        CGAL::Exact_predicates_exact_constructions_kernel.
    Use constants for size of grid, make it smaller for quicker tests!

--- cutcell.cpp
+++ cutcell.cpp
@@ -1,96 +1,63 @@
-#include <CGAL/Simple_cartesian.h>
-#include <CGAL/Polyhedron_3.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Nef_polyhedron_3.h>
-#include <CGAL/MP_Float.h>
-#include <CGAL/Quotient.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/IO/Nef_polyhedron_iostream_3.h>
+#include <fstream>
 #include <iostream>
 
-typedef CGAL::Filtered_kernel<
-        CGAL::Simple_cartesian<
-        CGAL::Quotient<
-        CGAL::MP_Float> > > Kernel;
+typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
 typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
 typedef CGAL::Nef_polyhedron_3<Kernel> Nef_polyhedron;
 typedef Polyhedron::Vertex_iterator Vertex_iterator;
 typedef Kernel::Vector_3 Vector;
 typedef Kernel::Aff_transformation_3 Aff_transformation;
 
-template <class Poly>
-typename Poly::Halfedge_handle make_cube_3( Poly& P) {
-    // appends a cube of size [0,1]^3 to the polyhedron P.
-    CGAL_precondition( P.is_valid());
-    typedef typename Poly::Point_3         Point;
-    typedef typename Poly::Halfedge_handle Halfedge_handle;
-    Halfedge_handle h = P.make_tetrahedron( Point( 1, 0, 0),
-                                            Point( 0, 0, 1),
-                                            Point( 0, 0, 0),
-                                            Point( 0, 1, 0));
-    Halfedge_handle g = h->next()->opposite()->next();             // Fig. (a)
-    P.split_edge( h->next());
-    P.split_edge( g->next());
-    P.split_edge( g);                                              // Fig. (b)
-    h->next()->vertex()->point()     = Point( 1, 0, 1);
-    g->next()->vertex()->point()     = Point( 0, 1, 1);
-    g->opposite()->vertex()->point() = Point( 1, 1, 0);            // Fig. (c)
-    Halfedge_handle f = P.split_facet( g->next(),
-                                       g->next()->next()->next()); // Fig. (d)
-    Halfedge_handle e = P.split_edge( f);
-    e->vertex()->point() = Point( 1, 1, 1);                        // Fig. (e)
-    P.split_facet( e, f->next()->next());                          // Fig. (f)
-    CGAL_postcondition( P.is_valid());
-    return h;
-}
-
 enum Cell { Solid, Fluid, Cut };
 
 int main() {
-    Polyhedron P;
+    Nef_polyhedron UnitCube;
+    const unsigned NX = 5, NY = 5, NZ = 5;
 
     std::cout << "Making cube" << std::endl;
-    make_cube_3(P);
+    std::ifstream in("cube.nef3"); // Load the definition of a unit cube from file
+    in >> UnitCube;
 
-    Nef_polyhedron N[10][10][10];
-    Nef_polyhedron N1;
-    Cell cell[10][10][10];
+    Nef_polyhedron N[NX][NY][NZ];
+    Nef_polyhedron N1(UnitCube);
+    Cell cell[NX][NY][NZ];
     std::cout << "Making grid" << std::endl;
-    for (int x = 0; x < 10; x++)
-        for (int y = 0; y < 10; y++)
-            for (int z = 0; z < 10; z++) {
-                N[x][y][z] = P;
+    for (int x = 0; x < NX; x++)
+        for (int y = 0; y < NY; y++)
+            for (int z = 0; z < NZ; z++) {
+                N[x][y][z] = UnitCube;
                 Aff_transformation Aff(CGAL::TRANSLATION, Vector(x,y,z));
                 N[x][y][z].transform(Aff);
             }
 
-    N1.convert_to_polyhedron(P);
-    //std::cout << N;
-    CGAL::set_ascii_mode( std::cout);
-    for (Vertex_iterator v = P.vertices_begin(); v != P.vertices_end(); ++v)
-        std::cout << v->point() << std::endl;
-    Aff_transformation Aff(2,0,0,0.5,
-                           0,2,0,0.5,
-                           0,0,2,0.5);
+    Aff_transformation Aff1(CGAL::TRANSLATION, Vector(0.5,0.5,0.5));
+    Aff_transformation Aff2(CGAL::SCALING, 1.5);
     std::cout << "Transforming..." << std::endl;
-    N1.transform(Aff);
-    N1.convert_to_polyhedron(P);
+    N1.transform(Aff1);
+    N1.transform(Aff2);
 
-    std::cout << "Computing Union" << std::endl;
-    for (int x = 0; x < 10; x++)
-        for (int y = 0; y < 10; y++)
-            for (int z = 0; z < 10; z++) {
-                Nef_polyhedron I = N1 * N[x][y][z];
-                if (z == 1) {
-                    I.convert_to_polyhedron(P);
-                }
+    std::cout << "Computing Union. Solid = " << Solid << ", Fluid = " << Fluid << ", Cut = " << Cut << std::endl;
+    for (int x = 0; x < NX; x++) {
+        for (int y = 0; y < NY; y++)
+            for (int z = 0; z < NZ; z++) {
+                Nef_polyhedron I = N[x][y][z] - N1;
                 if (I.is_empty())
-                    cell[x][y][z] = Fluid;
-                else if (I == N[x][y][z])
                     cell[x][y][z] = Solid;
+                else if (I == N[x][y][z])
+                    cell[x][y][z] = Fluid;
                 else
                     cell[x][y][z] = Cut;
                 std::cout << cell[x][y][z];
             }
+        std::cout << std::endl;
+    }
 
     if (N1.is_simple()) {
+        Polyhedron P;
         N1.convert_to_polyhedron(P);
         //std::cout << N;
         CGAL::set_ascii_mode( std::cout);
