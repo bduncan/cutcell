37419d357ae7da386118ec804feec09623401bc0
Bruce Duncan <Bruce.Duncan@ed.ac.uk>
2011-05-12 16:42:46 +0100

    Implement cut cell output.
    Output cut cells in CGNS format with the "TETRA_4" element type. This involves
    tetrahedralising the arbitrary Nef_polyhedron created in the cutting stage. To
    do this, we use the CGAL::Delaunay_triangulation_3 class. This class does not
    handle concave polyhedra, so we use the CGAL::convex_decomposition_3 class to
    ensure that each polyhedra is tetrahedralisable.
    
    Also, convert the fluid output routine to output HEXA_8 elements instead of
    arbitrary polyhedra. This is a lot simpler when everything is a cube.

--- cutcell.cpp
+++ cutcell.cpp
@@ -24,6 +24,9 @@
 #include <CGAL/IO/Polyhedron_VRML_2_ostream.h>
 #include <CGAL/centroid.h>
 #include <CGAL/Triangulation_3.h>
+#include <CGAL/Delaunay_triangulation_3.h>
+#include <CGAL/convex_decomposition_3.h>
+
 #include <list>
 #include <fstream>
 #include <iostream>
@@ -34,6 +37,7 @@
 namespace cutcell {
 
 typedef CGAL::Triangulation_3<cutcell::Kernel> Triangulation;
+typedef CGAL::Delaunay_triangulation_3<cutcell::Kernel> Delaunay_triangulation;
 
 Grid::Grid(int X, int Y, int Z) : N_(boost::extents[X][Y][Z]), cell_(boost::extents[X][Y][Z]) {
     // Create the Unit Cube from the global string definition.
@@ -189,7 +193,7 @@ int Grid::output_cgns_file(std::string const& name) const {
     int index_file = 0, index_base = 0, index_zone = 0;
     int index_coord_x = 0, index_coord_y = 0, index_coord_z = 0;
     int index_section = 0;
-    int cells = 0;
+    int hexa_8_cells = 0, tetra_4_cells = 0;
     if (cg_open(name.c_str(), CG_MODE_WRITE, &index_file) != CG_OK) {
         return 1;
     }
@@ -204,69 +208,155 @@ int Grid::output_cgns_file(std::string const& name) const {
     // Loop over all the Vertices (Points) in the grid and _uniquely_ add them
     // to the xvec, yvec, zvec vectors of the respective 3D coordinates.
     std::vector<double> xvec, yvec, zvec;
-    std::vector<int> elements;
+    std::vector<int> hexa_8_elements, tetra_4_elements;
     for (V3NefIndex x = 0; x < N_.shape()[0]; ++x)
         for (V3NefIndex y = 0; y < N_.shape()[1]; ++y)
             for (V3NefIndex z = 0; z < N_.shape()[2]; ++z) {
-                Nef_polyhedron::Halffacet_const_iterator fi;
-                CGAL_forall_facets(fi, N_[x][y][z]) {
-                    // circulate 3 halfedges => vertices
-                    //for (Nef_polyhedron::Halffacet_cycle_const_iterator hfi = fi->facet_cycles_begin(); hfi != fi->facet_cycles_end(); ++hfi) {
-                    Nef_polyhedron::Halffacet_cycle_const_iterator hfc;
-                    CGAL_forall_facet_cycles_of(hfc, fi) {
-                        int elements_end_index = elements.size();
-                        elements.push_back(NGON_n);
-                        size_t i = 0;
-                        assert(hfc.is_shalfedge());
-                        Nef_polyhedron::SHalfedge_const_handle se(hfc);
-                        Nef_polyhedron::SHalfedge_around_facet_const_circulator hc(se);
-                        Nef_polyhedron::SHalfedge_around_facet_const_circulator hc_end(hc);
-                        CGAL_For_all(hc, hc_end) {
-                            std::cerr << "SHalfedge from " << hc->source()->point() << std::endl;
-                            Nef_polyhedron::Point_3 point = hc->source()->point();
-                            std::vector<Nef_polyhedron::Point_3>::iterator it;
-                            for (it = points_3.begin(); it != points_3.end(); ++it)
-                                if (*it == point)
-                                    break;
-                            if (it == points_3.end()) { // if point not in points_3
-                                xvec.push_back(CGAL::to_double(point.x()));
-                                yvec.push_back(CGAL::to_double(point.y()));
-                                zvec.push_back(CGAL::to_double(point.z()));
-                                points_3.push_back(point);
-                                std::cerr << "Added point " << points_3[points_3.size() - 1] << ": " << xvec[xvec.size() - 1] << " " << yvec[yvec.size() - 1] << " " << zvec[zvec.size() - 1] << std::endl;
-                                it = points_3.end();
-                                --it;
+                if (cell_[x][y][z].type() == Fluid) {
+                    // This is a cube.
+                    int nodes = 0;
+                    bool first = true;
+                    Nef_polyhedron::Kernel::Direction_3 plane;
+                    Nef_polyhedron::Halffacet_const_iterator fi;
+                    int faces = 0;
+                    CGAL_forall_facets(fi, N_[x][y][z]) {
+                        bool reversed = false;
+                        ++faces;
+                        #ifndef NDEBUG
+                        std::cerr << "facet plane: " << fi->plane().orthogonal_direction() << "\topposite is: " << -fi->plane().orthogonal_direction() << std::endl;
+                        #endif
+                        if (first) {
+                            first = false;
+                            plane = fi->plane().orthogonal_direction();
+                        } else {
+                            if (plane != fi->plane().orthogonal_direction() && plane != -fi->plane().orthogonal_direction()) {
+                                continue;
                             }
-                            // Push the index of the element in the points_3 set into the connectivity list.
-                            int element_index = it - points_3.begin(); //std::distance(points_3.begin(), it);
-                            std::cerr << "element index: " << element_index << std::endl;
-                            elements.push_back(element_index);
-                            ++i;
+                            reversed = plane == -fi->plane().orthogonal_direction();
                         }
-                        std::cerr << "End of facet cycle " << cells << ". number of elements is " << (elements.size() - elements_end_index - 1) << std::endl;
-                        elements[elements_end_index] = NGON_n + elements.size() - elements_end_index - 1;
-                        ++cells;
+                        Nef_polyhedron::Halffacet_cycle_const_iterator hfc;
+                        CGAL_forall_facet_cycles_of(hfc, fi) {
+                            Nef_polyhedron::SHalfedge_const_handle se(hfc);
+                            Nef_polyhedron::SHalfedge_around_facet_const_circulator hc(se);
+                            Nef_polyhedron::SHalfedge_around_facet_const_circulator hc_end(hc);
+                            // Make sure the circulator starts at the vertex closest to the origin, so that we can ensure the two planes start at the correct vertices.
+                            Nef_polyhedron::Vector_3 min_distance = hc->source()->center_vertex()->point() - CGAL::ORIGIN;
+                            Nef_polyhedron::SHalfedge_around_facet_const_circulator min_se = hc;
+                            ++hc;
+                            CGAL_For_all(hc, hc_end) {
+                                Nef_polyhedron::Vector_3 distance = hc->source()->center_vertex()->point() - CGAL::ORIGIN;
+                                if (distance.squared_length() < min_distance.squared_length()) {
+                                    min_distance = distance;
+                                    min_se = hc;
+                                }
+                            }
+                            hc = min_se;
+                            #ifndef NDEBUG
+                            std::cerr << "Advanced hc by " << (&hc - &hc_end) << " places to start at " << hc->source()->center_vertex()->point() << std::endl;
+                            #endif
+                            hc_end = hc;
+                            // This is the unrolled macro CGAL_For_all from CGAL/circulator.h:348, modified to allow moving backwards.
+                            for (bool _circ_loop_flag = ! ::CGAL::is_empty_range( hc, hc_end); _circ_loop_flag; _circ_loop_flag = reversed ? ((hc) != (--hc_end)) : ((++hc) != (hc_end))) {
+                                Nef_polyhedron::Point_3 point((reversed ? hc_end : hc)->source()->center_vertex()->point());
+                                #ifndef NDEBUG
+                                std::cerr << "SHalfedge from " << point << std::endl;
+                                #endif
+                                std::vector<Nef_polyhedron::Point_3>::iterator it;
+                                for (it = points_3.begin(); it != points_3.end(); ++it)
+                                    if (*it == point)
+                                        break;
+                                if (it == points_3.end()) { // if point not in points_3
+                                    xvec.push_back(CGAL::to_double(point.x()));
+                                    yvec.push_back(CGAL::to_double(point.y()));
+                                    zvec.push_back(CGAL::to_double(point.z()));
+                                    points_3.push_back(point);
+                                    #ifndef NDEBUG
+                                    std::cerr << "Added point " << points_3[points_3.size() - 1] << ": " << xvec[xvec.size() - 1] << " " << yvec[yvec.size() - 1] << " " << zvec[zvec.size() - 1] << std::endl;
+                                    #endif
+                                    it = points_3.end();
+                                    --it;
+                                }
+                                // Push the index of the element in the points_3 set into the connectivity list.
+                                int element_index = std::distance(points_3.begin(), it);
+                                #ifndef NDEBUG
+                                std::cerr << "element index: " << element_index << std::endl;
+                                #endif
+                                hexa_8_elements.push_back(element_index + 1); // indices are 1-based.
+                                ++nodes;
+                            }
+                        }
+                    }
+                    ++hexa_8_cells;
+                    #ifndef NDEBUG
+                    std::cerr << "Facet plane had " << faces << " faces. Expected 6." << std::endl;
+                    std::cerr << x << " " << y << " " << z << " had " << nodes << " nodes." << std::endl;
+                    #endif
+                    if (nodes != 8) {
+                        abort();
+                    }
+                }
+                else if (cell_[x][y][z].type() == Cut) {
+                    #ifndef NDEBUG
+                    std::cerr << "Processing cut cell at " << x << " " << y << " " << z << std::endl;
+                    #endif
+                    Nef_polyhedron N(N_[x][y][z]);
+                    CGAL::convex_decomposition_3(N);
+                    // the first volume is the outer volume, which is ignored in the decomposition
+                    Nef_polyhedron::Volume_const_iterator vit = ++N.volumes_begin();
+                    for ( ; vit != N.volumes_end(); ++vit) {
+                        if (!vit->mark()) // FIXME: I don't know what this does...
+                            continue;
+                        Polyhedron P;
+                        N.convert_inner_shell_to_polyhedron(vit->shells_begin(), P);
+                        Delaunay_triangulation T(P.points_begin(), P.points_end());
+                        for (Delaunay_triangulation::Finite_cells_iterator it = T.finite_cells_begin(); it != T.finite_cells_end(); ++it) {
+                            for (int i = 0; i < 4; ++i) {
+                                Nef_polyhedron::Point_3 point = it->vertex(i)->point();
+                                std::vector<Nef_polyhedron::Point_3>::iterator it;
+                                for (it = points_3.begin(); it != points_3.end(); ++it)
+                                    if (*it == point)
+                                        break;
+                                if (it == points_3.end()) { // if point not in points_3
+                                    xvec.push_back(CGAL::to_double(point.x()));
+                                    yvec.push_back(CGAL::to_double(point.y()));
+                                    zvec.push_back(CGAL::to_double(point.z()));
+                                    points_3.push_back(point);
+                                    std::cerr << "Added point " << points_3[points_3.size() - 1] << ": " << xvec[xvec.size() - 1] << " " << yvec[yvec.size() - 1] << " " << zvec[zvec.size() - 1] << std::endl;
+                                    it = points_3.end();
+                                    --it;
+                                }
+                                // Push the index of the element in the points_3 set into the connectivity list.
+                                int element_index = std::distance(points_3.begin(), it);
+                                #ifndef NDEBUG
+                                std::cerr << "element index: " << element_index << std::endl;
+                                #endif
+                                tetra_4_elements.push_back(element_index + 1); // indices are 1-based.
+                            }
+                            ++tetra_4_cells;
+                        }
                     }
                 }
             }
-    int isize[3][3] = {0}; // TODO Why is isize 3*3?
+    int isize[9] = {0};
     // vertex size
-    isize[0][0] = points_3.size();
+    isize[0] = points_3.size();
     // cell size
-    assert(cells > 0);
-    isize[1][0] = cells;
+    assert(hexa_8_cells + tetra_4_cells > 0);
+    isize[1] = hexa_8_cells + tetra_4_cells;
     // boundary size
-    isize[2][0] = 0;
-    if (cg_zone_write(index_file, index_base, "Zone_1", reinterpret_cast<int*>(isize), Unstructured, &index_zone) != CG_OK) {
+    //isize[0][2] = 0;
+    if (cg_zone_write(index_file, index_base, "Zone1", isize, Unstructured, &index_zone) != CG_OK) {
         std::cerr << cg_get_error() << std::endl;
         (void)cg_close(index_file);
         return 1;
     }
-    std::cerr << "Writing coordinates..." << std::endl;
-    // We told cg_zone_write to expect N_.shape() vertex coordinates in each dimension.
-    assert(xvec.size() >= N_.shape()[0]);
-    assert(yvec.size() >= N_.shape()[1]);
-    assert(zvec.size() >= N_.shape()[2]);
+    #ifndef NDEBUG
+    std::cerr << "Writing " << xvec.size() << " coordinates..." << std::endl;
+    #endif
+    // We told cg_zone_write to expect isize[0] vertex coordinates in each dimension.
+    assert(xvec.size() >= isize[0]);
+    assert(yvec.size() >= isize[0]);
+    assert(zvec.size() >= isize[0]);
     if (cg_coord_write(index_file, index_base, index_zone, RealDouble, "CoordinateX", &xvec[0], &index_coord_x) != CG_OK ||
         cg_coord_write(index_file, index_base, index_zone, RealDouble, "CoordinateY", &yvec[0], &index_coord_y) != CG_OK ||
         cg_coord_write(index_file, index_base, index_zone, RealDouble, "CoordinateZ", &zvec[0], &index_coord_z) != CG_OK) {
@@ -274,11 +364,27 @@ int Grid::output_cgns_file(std::string const& name) const {
         (void)cg_close(index_file);
         return 1;
     }
-    std::cerr << "Writing elements..." << std::endl;
-    if (cg_section_write(index_file, index_base, index_zone, "GridElements", MIXED, 1, cells, /* nbndry = */ 0, &elements[0], &index_section) != CG_OK) {
-        std::cerr << cg_get_error() << std::endl;
-        (void)cg_close(index_file);
-        return 1;
+    if (hexa_8_cells > 0) {
+        #ifndef NDEBUG
+        std::cerr << "Writing " << hexa_8_cells << " Fluid elements..." << std::endl;
+        #endif
+        assert(hexa_8_elements.size() >= hexa_8_cells);
+        if (cg_section_write(index_file, index_base, index_zone, "FluidGridElements", HEXA_8, 1, hexa_8_cells, /* nbndry = */ 0, &hexa_8_elements[0], &index_section) != CG_OK) {
+            std::cerr << cg_get_error() << std::endl;
+            (void)cg_close(index_file);
+            return 1;
+        }
+    }
+    if (tetra_4_cells > 0) {
+        #ifndef NDEBUG
+        std::cerr << "Writing " << tetra_4_cells << " Cut cell elements..." << std::endl;
+        #endif
+        assert(tetra_4_elements.size() >= tetra_4_cells);
+        if (cg_section_write(index_file, index_base, index_zone, "CutGridElements", TETRA_4, 1, tetra_4_cells, /* nbndry = */ 0, &tetra_4_elements[0], &index_section) != CG_OK) {
+            std::cerr << cg_get_error() << std::endl;
+            (void)cg_close(index_file);
+            return 1;
+        }
     }
 
     if (cg_close(index_file) != CG_OK) {
